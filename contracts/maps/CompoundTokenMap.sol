b67d5bcfbee123a1d7bc049ba88f62368e1a4ffd
d1ll0n <dillonkellar@protonmail.com>
2021-05-31 10:54:36 -0500

    Refactor ceth and creth handling
    Use try/catch block for call to underlying instead of checking name hash

--- contracts/maps/CompoundTokenMap.sol
+++ contracts/maps/CompoundTokenMap.sol
@@ -7,11 +7,6 @@ import "../interfaces/CompoundInterfaces.sol";
 contract CompoundTokenMap {
   IComptroller public immutable comptroller;
   address public immutable weth;
-  bytes32 public constant compoundEtherNameHash = keccak256(
-    bytes(
-      string("Compound Ether")
-    )
-  );
   string public name = "Compound Token Map";
 
   mapping(address => address) public cTokens;
@@ -35,20 +30,19 @@ contract CompoundTokenMap {
   function mapAll() external {
     ICToken[] memory _cTokens = comptroller.getAllMarkets();
     uint256 len = _cTokens.length;
-    uint256 i = allUnderlyings.length;
-    bool foundEther = false;
-    while(!foundEther && i < len) {
-      ICToken cToken = _cTokens[i++];
-      if (keccak256(bytes(cToken.name())) == compoundEtherNameHash) {
-        foundEther = true;
-        cTokens[weth] = address(cToken);
-        allUnderlyings.push(weth);
-      } else {
-        mapCToken(cToken);
+    for (uint256 i = allUnderlyings.length; i < len; i++) {
+      ICToken cToken = _cTokens[i];
+      address underlying;
+      // The call to underlying will use all the gas sent if it fails,
+      // so we specify a maximum of 25k gas. The contract will only use ~2k
+      // but this protects against all likely changes to the gas schedule.
+      try cToken.underlying{gas: 25000}() returns (address _underlying) {
+        underlying = _underlying;
+      } catch {
+        underlying = weth;
       }
-    }
-    for (; i < len; i++) {
-      mapCToken(_cTokens[i]);
+      cTokens[underlying] = address(cToken);
+      allUnderlyings.push(underlying);
     }
   }
 }
