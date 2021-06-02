73bb706bc5b9133b895e9b9f26bf9af18533d292
d1ll0n <dillonkellar@protonmail.com>
2021-06-02 12:06:44 -0500

    Separate abstract adapters into their own files

---
+++ contracts/adapters/AbstractEtherAdapter.sol
@@ -0,0 +1,76 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.7.6;
+
+import "../libraries/TransferHelper.sol";
+import "../interfaces/IERC20Metadata.sol";
+import "../interfaces/IERC20.sol";
+import "./AbstractErc20Adapter.sol";
+
+
+abstract contract AbstractEtherAdapter is AbstractErc20Adapter {
+  using TransferHelper for address;
+
+/* ========== Metadata ========== */
+
+  function name() external view virtual override returns (string memory) {
+    return string(abi.encodePacked(
+      bytes(_protocolName()),
+      " Ether Adapter"
+    ));
+  }
+
+/* ========== Token Actions ========== */
+
+  function deposit(uint256 amountUnderlying) external virtual override returns (uint256 amountMinted) {
+    underlying.safeTransferFrom(msg.sender, address(this), amountUnderlying);
+    _afterReceiveWETH(amountUnderlying);
+    amountMinted = _mint(amountUnderlying);
+    token.safeTransfer(msg.sender, amountMinted);
+  }
+
+  function depositETH() external virtual payable returns (uint256 amountMinted) {
+    _afterReceiveETH(msg.value);
+    amountMinted = _mint(msg.value);
+    token.safeTransfer(msg.sender, amountMinted);
+  }
+
+  function withdraw(uint256 amountToken) external virtual override returns (uint256 amountReceived) {
+    token.safeTransferFrom(msg.sender, address(this), amountToken);
+    amountReceived = _burn(amountToken);
+    _beforeSendWETH(amountReceived);
+    underlying.safeTransfer(msg.sender, amountReceived);
+  }
+
+  function withdrawAsETH(uint256 amountToken) external virtual returns (uint256 amountReceived) {
+    token.safeTransferFrom(msg.sender, address(this), amountToken);
+    amountReceived = _burn(amountToken);
+    _beforeSendETH(amountReceived);
+    address(msg.sender).safeTransferETH(amountReceived);
+  }
+
+  function withdrawUnderlying(uint256 amountUnderlying) external virtual override returns (uint256 amountBurned) {
+    amountBurned = _burnUnderlying(amountUnderlying);
+    _beforeSendWETH(amountUnderlying);
+    underlying.safeTransfer(msg.sender, amountUnderlying);
+  }
+
+  function withdrawUnderlyingAsETH(uint256 amountUnderlying) external virtual returns (uint256 amountBurned) {
+    amountBurned = _burnUnderlying(amountUnderlying);
+    _beforeSendETH(amountUnderlying);
+    address(msg.sender).safeTransferETH(amountUnderlying);
+  }
+
+/* ========== Internal Ether Handlers ========== */
+  
+  // Convert to WETH if contract takes WETH
+  function _afterReceiveETH(uint256 amount) internal virtual;
+
+  // Convert to WETH if contract takes ETH
+  function _afterReceiveWETH(uint256 amount) internal virtual;
+
+  // Convert to ETH if contract returns WETH
+  function _beforeSendETH(uint256 amount) internal virtual;
+
+  // Convert to WETH if contract returns ETH
+  function _beforeSendWETH(uint256 amount) internal virtual;
+}
