16bbd90cb2d66167eab82491566fcd7a7437afce
d1ll0n <dillonkellar@protonmail.com>
2021-06-04 08:48:35 -0500

    add support for bytes32 erc20 symbols

--- contracts/interfaces/IERC20Metadata.sol
+++ contracts/interfaces/IERC20Metadata.sol
@@ -7,3 +7,9 @@ interface IERC20Metadata {
   function symbol() external view returns (string memory);
   function decimals() external view returns (uint8);
 }
+
+
+interface IERC20MetadataBytes32 {
+  function name() external view returns (bytes32);
+  function symbol() external view returns (bytes32);
+}
