e0f7e1504f91c4a9fb2ad495dc6eb6952351cae3
d1ll0n <dillonkellar@protonmail.com>
2021-06-06 13:33:11 -0500

    add totalAssetSupply and totalSupplyInterestRate

--- contracts/interfaces/FulcrumInterfaces.sol
+++ contracts/interfaces/FulcrumInterfaces.sol
@@ -11,6 +11,8 @@ interface IBZX {
 
 interface IToken {
   function supplyInterestRate() external view returns (uint256);
+  function totalAssetSupply() external view returns (uint256);
+  function totalSupplyInterestRate(uint256 assetSupply) external view returns (uint256);
   function nextSupplyInterestRate(uint256 supplyAmount) external view returns (uint256);
   function mint(address receiver, uint256 amount) external payable returns (uint256 mintAmount);
   function burn(address receiver, uint256 burnAmount) external returns (uint256 loanAmountPaid);
