490bbc478b32ea69004a11d1a36d0106a378c027
d1ll0n <dillonkellar@protonmail.com>
2021-06-07 03:51:11 -0500

    add isFrozen

--- contracts/libraries/ReserveConfigurationLib.sol
+++ contracts/libraries/ReserveConfigurationLib.sol
@@ -6,6 +6,7 @@ import "../interfaces/AaveV2Interfaces.sol";
 
 library ReserveConfigurationLib {
   uint256 internal constant RESERVE_FACTOR_MASK = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF0000FFFFFFFFFFFFFFFF; // prettier-ignore
+  uint256 constant FROZEN_MASK                  = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFDFFFFFFFFFFFFFF; // prettier-ignore
   uint256 internal constant RESERVE_FACTOR_START_BIT_POSITION = 64;
 
   /**
@@ -19,5 +20,13 @@ library ReserveConfigurationLib {
     returns (uint256)
   {
     return (self.data & ~RESERVE_FACTOR_MASK) >> RESERVE_FACTOR_START_BIT_POSITION;
+  }
+
+  function isFrozen(ILendingPool.ReserveConfigurationMap memory self)
+    internal
+    pure
+    returns (bool)
+  {
+    return (self.data & ~FROZEN_MASK) != 0;
   }
 }
