c01037c583505795d0ef717e5ba0144c2a0c90dc
d1ll0n <dillonkellar@protonmail.com>
2021-06-21 01:24:20 -0500

    Add explanation of test mapper

--- test/TestMapper.ts
+++ test/TestMapper.ts
@@ -1,8 +1,12 @@
 import { TestMapper } from '../typechain'
 
-import { deployContract } from "./shared"
+import { advanceBlock, deployContract, getBigNumber, sendTokenTo } from "./shared"
 
 
+/**
+ * This test file is used to automatically generate tests for all the tokens
+ * in each protocols.
+ */
 describe('Map Protocols', () => {
   let mapper: TestMapper
 
@@ -22,11 +26,48 @@ describe('Map Protocols', () => {
   //   await mapper.compound()
   // })
 
+/*   it('Cream', async () => {
+    await mapper.cream()
+  }) */
+
   // it('Fulcrumm', async () => {
   //   await mapper.fulcrum()
   // })
 
-  it('Yearn', async () => {
-    await mapper.yearn()
-  })
+  // it('Fuse', async () => {
+  //   await mapper.fuse()
+  // })
+
+  // it('Yearn', async () => {
+    // await mapper.yearn()
+  // })
+
+  // it('Aave V2', async () => {
+  //   const test = await deployContract('TestAaveV2');
+  //   await test.testRewardValue()
+  // })
+
+  // it('Compound', async () => {
+  //   const test = await deployContract('TestCompound');
+  //   await advanceBlock()
+  //   await test.testExchangeRate()
+  // })
+
+  // it('Cream', async () => {
+  //   const test = await deployContract('TestCompound');
+  //   await advanceBlock()
+  //   await sendTokenTo('0xaaaebe6fe48e54f431b0c390cfaf0b017d09d42d', test.address, getBigNumber(1, 4))
+  //   await test.testExchangeRateCream()
+  // })
+
+  // it('Iron Bank', async () => {
+  //     const test = await deployContract('TestCompound');
+  //     await advanceBlock()
+  //     await sendTokenTo('0x6B175474E89094C44Da98b954EedeAC495271d0F', test.address, getBigNumber(1, 18))
+  //     await test.testExchangeRateIronBank()
+  // })
+
+  // it('Iron Bank', async () => {
+  //   await mapper.iron()
+  // })
 })
