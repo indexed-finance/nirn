43b49ccd7d0b2f028055b52fdfa8bf9897727982
d1ll0n <dillonkellar@protonmail.com>
2021-06-21 00:43:13 -0500

    Add helper library for computing fractions of 1e18

---
+++ contracts/libraries/Fraction.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.7.6;
+
+import "../libraries/LowGasSafeMath.sol";
+
+
+library Fraction {
+  using LowGasSafeMath for uint256;
+
+  uint256 internal constant ONE_E18 = 1e18;
+
+  function mulFractionE18(uint256 a, uint256 fraction) internal pure returns (uint256) {
+    return a.mul(fraction) / ONE_E18;
+  }
+
+  function toFractionE18(uint256 a, uint256 b) internal pure returns (uint256) {
+    return a.mul(ONE_E18) / b;
+  }
+}
