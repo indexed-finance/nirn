b90d34fd5e0bf2d16a3e2f1e0c3ae8f975fef0d6
d1ll0n <dillonkellar@protonmail.com>
2021-06-21 01:12:08 -0500

    Add additional test utils for token acquisition and queries

--- test/shared/utils.ts
+++ test/shared/utils.ts
@@ -1,10 +1,11 @@
-import { getCreate2Address } from '@ethersproject/address';
+import { getAddress, getContractAddress, getCreate2Address } from '@ethersproject/address';
 import { JsonRpcSigner } from '@ethersproject/providers';
 import { keccak256 } from '@ethersproject/keccak256';
 import { BigNumber, Contract } from 'ethers';
-import { ethers, network } from 'hardhat';
-import { IERC20, IWETH, TestFactory } from '../../typechain';
+import { ethers, network, waffle } from 'hardhat';
+import { IERC20, IVault, IWETH, IYearnRegistry, TestFactory, IUniswapV2Pair, IERC20Metadata } from '../../typechain';
 import { addProxy } from './proxyResolution';
+import { formatUnits, parseUnits } from '@ethersproject/units';
 
 export const SUSHISWAP_FACTORY_ADDRESS = '0xC0AEe478e3658e2610c5F7A4A2E1777cE9e4f2Ac'
 export const UNISWAP_FACTORY_ADDRESS = '0x5c69bee701ef814a2b6a3edd4b1652cb9cc5aa6f';
@@ -18,6 +19,11 @@ export function getBigNumber(n: number, decimals = 18) {
 export async function getContractBase<C extends Contract>(address: string, name: string): Promise<C> {
   let contract = await ethers.getContractAt(name, address);
   return contract as C;
+}
+
+export async function getNextContractAddress(account: string): Promise<string> {
+  const nonce = await ethers.provider.getTransactionCount(account);
+  return getContractAddress({ from: account, nonce });
 }
 
 //#region Fork utils
@@ -42,8 +48,8 @@ export async function resetFork() {
     method: 'hardhat_reset',
     params: [{
       forking: {
-        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
-        blockNumber: 12569699
+        jsonRpcUrl: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+        blockNumber: 12667185
       }
     }]
   })
@@ -57,11 +63,47 @@ export async function withSigner(address: string, fn: (signer: JsonRpcSigner) =>
   await stopImpersonating(address);
 }
 
-export const sendEtherTo = (address: string, amount: BigNumber = BigNumber.from(10).pow(20)) => withSigner(WETH, async (signer) => {
+export const sendEtherTo = (address: string, amount: BigNumber = getBigNumber(1)) => withSigner(WETH, async (signer) => {
   const factory = await ethers.getContractFactory('SendEth');
   const tx = await factory.getDeployTransaction(address);
   await signer.sendTransaction({ data: tx.data, value: amount });
 });
+
+export async function getIERC20(token: string): Promise<IERC20> {
+  return getContract(token, 'IERC20')
+}
+
+export async function getBalance(erc20: string, account: string): Promise<BigNumber> {
+  return (await getIERC20(erc20)).balanceOf(account);
+}
+
+const holders: Record<string, string> = {
+  '0xbA4cFE5741b357FA371b506e5db0774aBFeCf8Fc': '0xDBC13E67F678Cc00591920ceCe4dCa6322a79AC7'
+}
+
+export async function isPairToken(token: string) {
+  return (await getContract(token, 'IUniswapV2Pair')).getReserves().then(() => true).catch(() => false)
+}
+
+export async function sendPairTokens(pairAddress: string, to: string, amount: BigNumber) {
+  const pair: IUniswapV2Pair = await getContract(pairAddress, 'IUniswapV2Pair');
+  const t0 = await getIERC20(await pair.token0());
+  const t1 = await getIERC20(await pair.token1());
+  const b0 = await t0.balanceOf(pairAddress);
+  const b1 = await t1.balanceOf(pairAddress);
+  const supply = await pair.totalSupply();
+  const a0 = amount.mul(b0).div(supply);
+  const a1 = amount.mul(b1).div(supply);
+  const [wallet] = waffle.provider.getWallets()
+  await sendTokenTo(t0.address, wallet.address, a0);
+  await sendTokenTo(t1.address, wallet.address, a1);
+  await pair.sync();
+  await t0.transfer(pair.address, a0)
+  await t1.transfer(pair.address, a1)
+  await withSigner(WETH, async (signer) => {
+    await pair.connect(signer).mint(to);
+  })
+}
 
 export async function sendTokenTo(erc20: string, to: string, amount: BigNumber) {
   if (erc20.toLowerCase() === WETH.toLowerCase()) {
@@ -71,12 +113,23 @@ export async function sendTokenTo(erc20: string, to: string, amount: BigNumber) 
       await weth.deposit({ value: amount });
     })
   } else {
+    const token = (await ethers.getContractAt('IERC20', erc20)) as IERC20;
     let pair = computeUniswapPairAddress(erc20, WETH);
     const code = await ethers.provider.getCode(pair)
-    if (code === '0x') {
+    if (code === '0x' || (await token.balanceOf(pair)).lt(amount)) {
       pair = computeSushiswapPairAddress(erc20, WETH);
     }
-    const token = (await ethers.getContractAt('IERC20', erc20)) as IERC20;
+    if ((await token.balanceOf(pair)).lt(amount)) {
+      if (holders[getAddress(erc20)]) {
+        pair = holders[getAddress(erc20)]
+      } else {
+        if (await isPairToken(erc20)) {
+          await sendPairTokens(erc20, to, amount);
+          return;
+        }
+        throw new Error('Could not find holder to transfer tokens from');
+      }
+    }
     await sendEtherTo(pair);
     await withSigner(pair, async (signer) => {
       await token.connect(signer).transfer(to, amount);
@@ -116,7 +169,12 @@ export async function deployClone<C extends Contract>(implementation: C, name: s
   return new Contract(address, implementation.interface, implementation.signer) as C;
 }
 
+const toAddressBuffer = (address: string) => Buffer.from(address.slice(2).padStart(40, "0"), "hex");
 //#region Uniswap
+
+export const INIT_CODE_HASH_SUSHI = '0xe18a34eb0e04b04f7a0ac29a6e80748dca96319b42c54d679cb821dca90c6303'
+export const INIT_CODE_HASH_UNI = '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f'
+
 export function sortTokens(tokenA: string, tokenB: string): string[] {
   return tokenA.toLowerCase() < tokenB.toLowerCase()
     ? [tokenA, tokenB]
@@ -127,13 +185,12 @@ export function computeUniswapPairAddress(
   tokenA: string,
   tokenB: string
 ): string {
-  const initCodeHash =
-    "0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f";
+  const initCodeHash = INIT_CODE_HASH_UNI;
   const [token0, token1] = sortTokens(tokenA, tokenB);
   const salt = keccak256(
     Buffer.concat([
-      Buffer.from(token0.slice(2).padStart(40, "0"), "hex"),
-      Buffer.from(token1.slice(2).padStart(40, "0"), "hex"),
+      toAddressBuffer(token0),
+      toAddressBuffer(token1),
     ])
   );
   return getCreate2Address(UNISWAP_FACTORY_ADDRESS, salt, initCodeHash);
@@ -143,15 +200,53 @@ export function computeSushiswapPairAddress(
   tokenA: string,
   tokenB: string
 ): string {
-  const initCodeHash =
-    "0xe18a34eb0e04b04f7a0ac29a6e80748dca96319b42c54d679cb821dca90c6303";
+  const initCodeHash = INIT_CODE_HASH_SUSHI;
   const [token0, token1] = sortTokens(tokenA, tokenB);
   const salt = keccak256(
     Buffer.concat([
-      Buffer.from(token0.slice(2).padStart(40, "0"), "hex"),
-      Buffer.from(token1.slice(2).padStart(40, "0"), "hex"),
+      toAddressBuffer(token0),
+      toAddressBuffer(token1),
     ])
   );
   return getCreate2Address(SUSHISWAP_FACTORY_ADDRESS, salt, initCodeHash);
 }
 //#endregion Uniswap
+
+//#region token ammounts
+export async function getTokenDecimals(token: IERC20) {
+  const metadata: IERC20Metadata = await getContract(token.address, 'IERC20Metadata');
+  return metadata.decimals();
+}
+
+export async function getTokenSymbol(token: IERC20) {
+  console.log(`GET SYMMBOL ${token.address}`)
+  const metadata: IERC20Metadata = await getContract(token.address, 'IERC20Metadata');
+  return metadata.symbol();
+}
+
+export async function formatTokenAmount(token: IERC20, amount: BigNumber) {
+  return formatUnits(amount, await getTokenDecimals(token));
+}
+
+export async function parseTokenAmount(token: IERC20, amount: number) {
+  return parseUnits(amount.toString(10), await getTokenDecimals(token));
+}
+
+export async function getTokenDecimalsAndSymbol(token: IERC20) {
+  const metadata: IERC20Metadata = await getContract(token.address, 'IERC20Metadata');
+  const [decimals, symbol] = await Promise.all([
+    metadata.decimals(),
+    metadata.symbol()
+  ]);
+  return { decimals, symbol };
+}
+
+export async function getTokenAmountString(token: IERC20, amount: BigNumber) {
+  const metadata: IERC20Metadata = await getContract(token.address, 'IERC20Metadata');
+  const [decimals, symbol] = await Promise.all([
+    metadata.decimals(),
+    metadata.symbol()
+  ]);
+  return `${formatUnits(amount, decimals)} ${symbol}`;
+}
+//#endregion
