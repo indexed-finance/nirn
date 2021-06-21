591896f3cbacce81d70bd2475830a99757591bb4
d1ll0n <dillonkellar@protonmail.com>
2021-06-21 01:18:44 -0500

    Remove use of cToken adapter factory, deploy adapters directly

--- contracts/protocols/CompoundProtocolAdapter.sol
+++ contracts/protocols/CompoundProtocolAdapter.sol
@@ -3,32 +3,34 @@ pragma solidity =0.7.6;
 
 import "../interfaces/CompoundInterfaces.sol";
 import "../interfaces/IAdapterRegistry.sol";
-import "./CTokenAdapterFactory.sol";
+import "../adapters/compound/CErc20Adapter.sol";
+import "../adapters/compound/CEtherAdapter.sol";
+import "../libraries/CloneLibrary.sol";
 
 
+// @todo Add freezing & unfreezing of adapters and tokens
 contract CompoundProtocolAdapter {
+  address public constant weth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
   IComptroller public constant comptroller = IComptroller(0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B);
   IAdapterRegistry public immutable registry;
-  CTokenAdapterFactory public immutable adapterFactory;
+  address public immutable erc20AdapterImplementation;
+  address public immutable etherAdapterImplementation;
 
   string public protocol = "Compound";
-  // @todo Add support for cBAT
   uint256 public totalMapped = 1;
   address[] public adapters;
   address[] public frozen;
 
-  constructor(
-    IAdapterRegistry _registry,
-    CTokenAdapterFactory _adapterFactory
-  ) {
+  constructor(IAdapterRegistry _registry) {
     registry = _registry;
-    adapterFactory = _adapterFactory;
+    erc20AdapterImplementation = address(new CErc20Adapter());
+    etherAdapterImplementation = address(new CEtherAdapter());
   }
 
   function unfreeze(uint256 i) external {
     ICToken cToken = ICToken(frozen[i]);
     require(!comptroller.mintGuardianPaused(address(cToken)), "Asset frozen");
-    (, address adapter) = adapterFactory.deployAdapter(cToken, "Compound");
+    (, address adapter) = deployAdapter(cToken);
     registry.addTokenAdapter(adapter);
     adapters.push(adapter);
     address last = frozen[frozen.length - 1];
@@ -69,10 +71,30 @@ contract CompoundProtocolAdapter {
         skipped++;
         continue;
       }
-      (,_adapters[i - skipped]) = adapterFactory.deployAdapter(cToken, "Compound");
+      (,_adapters[i - skipped]) = deployAdapter(cToken);
     }
     totalMapped += len;
     assembly { if gt(skipped, 0) { mstore(_adapters, sub(mload(_adapters), skipped)) } }
     registry.addTokenAdapters(_adapters);
+  }
+
+  function deployAdapter(ICToken cToken) internal returns (address underlying, address adapter) {
+    // The call to underlying will use all the gas sent if it fails,
+    // so we specify a maximum of 25k gas. The contract will only use ~2k
+    // but this protects against all likely changes to the gas schedule.
+    try cToken.underlying{gas: 25000}() returns (address _underlying) {
+      underlying = _underlying;
+      if (underlying == address(0)) {
+        underlying = weth;
+      }
+    } catch {
+      underlying = weth;
+    }
+    if (underlying == weth) {
+      adapter = CloneLibrary.createClone(etherAdapterImplementation);
+    } else {
+      adapter = CloneLibrary.createClone(erc20AdapterImplementation);
+    }
+    CErc20Adapter(adapter).initialize(underlying, address(cToken));
   }
 }
