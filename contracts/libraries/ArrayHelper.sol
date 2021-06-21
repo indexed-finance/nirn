fdbcb75f7d447c3465c7a4b1dfecd15723604b3e
d1ll0n <dillonkellar@protonmail.com>
2021-06-21 00:40:03 -0500

    Add ArrayHelper library

---
+++ contracts/libraries/ArrayHelper.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.5.0;
+
+import "@openzeppelin/contracts/utils/EnumerableSet.sol";
+
+
+library ArrayHelper {
+  using EnumerableSet for EnumerableSet.AddressSet;
+
+  function toArray(EnumerableSet.AddressSet storage set) internal view returns (address[] memory arr) {
+    bytes32[] memory bytes32Arr = set._inner._values;
+    assembly { arr := bytes32Arr }
+  }
+
+  /**
+   * @dev Remove the element at `index` from an array and decrement its length.
+   * If `index` is the last index in the array, pops it from the array.
+   * Otherwise, stores the last element in the array at `index` and then pops the last element.
+   */
+  function remove(address[] storage arr, uint256 index) internal {
+    uint256 len = arr.length;
+    if (index == len - 1) {
+      arr.pop();
+      return;
+    }
+    address last = arr[len - 1];
+    arr[index] = last;
+    arr.pop();
+  }
+
+  /**
+   * @dev Find the index of an address in an array.
+   * If the address is not found, revert.
+   */
+  function indexOf(address[] memory arr, address find) internal pure returns (uint256) {
+    uint256 len = arr.length;
+    for (uint256 i; i < len; i++) if (arr[i] == find) return i;
+    revert("Element not found in array");
+  }
+
+
+  /**
+   * @dev Given an array of tokens and scores, sort by scores in descending order.
+   */
+  function sortByDescendingScore(
+    address[] memory tokens,
+    uint256[] memory scores
+  ) internal pure {
+    uint256 len = tokens.length;
+    for (uint256 i = 0; i < len; i++) {
+      uint256 score = scores[i];
+      address token = tokens[i];
+      uint256 j = i - 1;
+      while (int(j) >= 0 && scores[j] < score) {
+        scores[j + 1] = scores[j];
+        tokens[j + 1] = tokens[j];
+        j--;
+      }
+      scores[j + 1] = score;
+      tokens[j + 1] = token;
+    }
+  }
+}
