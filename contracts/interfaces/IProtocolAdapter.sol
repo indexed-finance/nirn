73a16ff5fe5f83926cbc1c7345d7d1a75e7d019b
d1ll0n <dillonkellar@protonmail.com>
2021-06-22 00:41:09 -0500

    add functions to protocol adapter

--- contracts/interfaces/IProtocolAdapter.sol
+++ contracts/interfaces/IProtocolAdapter.sol
@@ -3,4 +3,6 @@ pragma solidity >=0.5.10;
 
 interface IProtocolAdapter {
   function protocol() external view returns (string memory);
+  function getUnmapped() external view returns (address[] memory tokens);
+  function map(uint256 max) external;
 }
