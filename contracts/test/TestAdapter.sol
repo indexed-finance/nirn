36ec85d0848f961cd8faf17afee02ae0b39c260c
d1ll0n <dillonkellar@protonmail.com>
2021-06-24 20:13:57 -0500

    Make test adapter more like real adapters

--- contracts/test/TestAdapter.sol
+++ contracts/test/TestAdapter.sol
@@ -8,20 +8,18 @@ import "../libraries/MinimalSignedMath.sol";
 contract TestAdapter {
   using LowGasSafeMath for uint256;
   using MinimalSignedMath for uint256;
+  using TransferHelper for address;
 
   address public immutable underlying;
   address public immutable token;
   uint256 internal annualInterest;
 
-  constructor(
-    address _underlying,
-    uint256 _annualInterest
-  ) {
-    // address _underlying = address(new TestERC20(_name, _symbol, _initialBalance));
-    
+  constructor(address _underlying, uint256 _annualInterest) {
     underlying = _underlying;
-    token = address(new TestVault(_underlying));
+    address _token = address(new TestVault(_underlying));
+    token = _token;
     annualInterest = _annualInterest;
+    _underlying.safeApproveMax(_token);
   }
 
   function toWrappedAmount(uint256 underlyingAmount) public view returns (uint256) {
@@ -54,5 +52,33 @@ contract TestAdapter {
 
   function balanceUnderlying() external view returns (uint256) {
     return toUnderlyingAmount(balanceWrapped());
+  }
+
+    function deposit(uint256 amountUnderlying) external virtual returns (uint256 amountMinted) {
+    require(amountUnderlying > 0, "deposit 0");
+    underlying.safeTransferFrom(msg.sender, address(this), amountUnderlying);
+    amountMinted = TestVault(token).deposit(amountUnderlying);
+    token.safeTransfer(msg.sender, amountMinted);
+  }
+
+  function withdraw(uint256 amountToken) public virtual returns (uint256 amountReceived) {
+    require(amountToken > 0, "withdraw 0");
+    token.safeTransferFrom(msg.sender, address(this), amountToken);
+    amountReceived = TestVault(token).withdraw(amountToken);
+    underlying.safeTransfer(msg.sender, amountReceived);
+  }
+
+  function withdrawAll() public virtual returns (uint256 amountReceived) {
+    return withdraw(balanceWrapped());
+  }
+
+  function withdrawUnderlying(uint256 amountUnderlying) external returns (uint256 amountBurned) {
+    TestVault vault = TestVault(token);
+    uint256 bal = vault.balance();
+    uint256 supply = vault.totalSupply();
+    amountBurned = amountUnderlying.mul(supply).divCeil(bal);
+    token.safeTransferFrom(msg.sender, address(this), amountBurned);
+    TestVault(token).withdraw(amountBurned);
+    underlying.safeTransfer(msg.sender, amountUnderlying);
   }
 }
