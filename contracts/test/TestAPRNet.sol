0da79c0fa22f02061434bf90a3c401bb3b410ee1
d1ll0n <dillonkellar@protonmail.com>
2021-06-24 20:13:30 -0500

    remove TestAdapter from TestAPRNet

--- contracts/test/TestAPRNet.sol
+++ contracts/test/TestAPRNet.sol
@@ -3,42 +3,6 @@ pragma solidity ^0.7.6;
 
 import "../libraries/APRNet.sol";
 
-contract TestAdapter {
-  using LowGasSafeMath for uint256;
-
-  address public underlying;
-  address public token;
-  uint256 internal immutable annualRewards;
-  uint256 internal immutable testBalance;
-  uint256 internal immutable testLiquidity;
-
-  constructor(
-    address _underlying,
-    address _token,
-    uint256 _annualRewards,
-    uint256 _testLiquidity,
-    uint256 _testBalance
-  ) {
-    underlying = _underlying;
-    token = _token;
-    annualRewards = _annualRewards;
-    testBalance = _testBalance;
-    testLiquidity = _testLiquidity;
-  }
-
-  function getAPR() external view returns (uint256) {
-    return annualRewards.mul(1e18) / testLiquidity;
-  }
-
-  function getHypotheticalAPR(int256 liquidityDelta) external view returns (uint256) {
-    return annualRewards.mul(1e18) / MinimalSignedMath.add(testLiquidity, liquidityDelta);
-  }
-
-  function balanceUnderlying() external view returns (uint256) {
-    return testBalance;
-  }
-}
-
 
 contract TestAPRNet {
   function validateWeights(uint256[] memory weights) internal pure {
