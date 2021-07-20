419f9176657d3747bb8ce35cde6fe020089d4dfb
d1ll0n <dillonkellar@protonmail.com>
2021-07-20 04:33:58 -0500

    Refactor test fixtures

--- test/shared/fixtures.ts
+++ test/shared/fixtures.ts
@@ -1,28 +1,44 @@
-import { BigNumber } from "ethers";
+import { BigNumber, constants } from "ethers";
 import { waffle } from "hardhat";
 import { AdapterRegistry, TestAdapter, TestERC20, TestVault } from "../../typechain";
 import { deployContract, getBigNumber, getContract } from "./utils";
 
-export async function deployTestAdaptersAndRegistry(name: string = 'Test Token', symbol: string = 'TOK', initBalance: BigNumber = getBigNumber(100)) {
-  const [wallet,,protocolAdapter] = waffle.provider.getWallets()
+export async function deployTestWrapperAndAdapter(
+  underlying: string,
+  apr: BigNumber = getBigNumber(1, 17)
+) {
+  const adapter: TestAdapter = await deployContract('TestAdapter', underlying, apr) // 10% apr
+  const wrapper: TestVault = await getContract(await adapter.token(), 'TestVault')
+  return { adapter, wrapper };
+}
+
+export function deployTestERC20(name: string = 'Test Token', symbol: string = 'TOK', initBalance: BigNumber = getBigNumber(100)): Promise<TestERC20> {
+  return deployContract('TestERC20', name, symbol, initBalance);
+}
+
+export async function deployTestAdaptersAndRegistry(name: string = 'Test Token', symbol: string = 'TOK', initBalance: BigNumber = getBigNumber(150)) {
+  const [,,protocolAdapter] = waffle.provider.getWallets()
   const underlying: TestERC20 = await deployContract('TestERC20', name, symbol, initBalance);
-  const adapter1: TestAdapter = await deployContract('TestAdapter', underlying.address, getBigNumber(1, 17)) // 10% apr
-  const adapter2: TestAdapter = await deployContract('TestAdapter', underlying.address, getBigNumber(5, 16)) // 5% apr
-  const wrapper1: TestVault = await getContract(await adapter1.token(), 'TestVault')
-  const wrapper2: TestVault = await getContract(await adapter2.token(), 'TestVault')
-  await underlying.approve(adapter1.address, initBalance.div(2))
-  await underlying.approve(adapter2.address, initBalance.div(2))
-  await adapter1.deposit(initBalance.div(2))
-  await adapter2.deposit(initBalance.div(2))
+  const { adapter: adapter1, wrapper: wrapper1 } = await deployTestWrapperAndAdapter(underlying.address, getBigNumber(1, 17))
+  const { adapter: adapter2, wrapper: wrapper2 } = await deployTestWrapperAndAdapter(underlying.address, getBigNumber(5, 16))
+  const { adapter: adapter3, wrapper: wrapper3 } = await deployTestWrapperAndAdapter(underlying.address, getBigNumber(1, 16))
+  await underlying.approve(adapter1.address, constants.MaxUint256)
+  await underlying.approve(adapter2.address, constants.MaxUint256)
+  await underlying.approve(adapter3.address, constants.MaxUint256)
+  await adapter1.deposit(initBalance.div(3))
+  await adapter2.deposit(initBalance.div(3))
+  await adapter3.deposit(initBalance.div(3))
   const registry: AdapterRegistry = await deployContract('AdapterRegistry')
   await registry.addProtocolAdapter(protocolAdapter.address)
-  await registry.connect(protocolAdapter).addTokenAdapters([ adapter1.address, adapter2.address ])
+  await registry.connect(protocolAdapter).addTokenAdapters([ adapter1.address, adapter2.address, adapter3.address ])
   return {
     underlying,
     adapter1,
     adapter2,
+    adapter3,
     wrapper1,
     wrapper2,
+    wrapper3,
     registry
   }
 }
