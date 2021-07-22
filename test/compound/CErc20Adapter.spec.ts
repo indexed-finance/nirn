91398d1de62f1f4073c0ba1ca812ed535a804c04
d1ll0n <dillonkellar@protonmail.com>
2021-07-22 01:02:20 -0500

    Rewrite withdrawal tests for comp adapters
    Needed to calculate correct expected balance for cCOMP

--- test/compound/CErc20Adapter.spec.ts
+++ test/compound/CErc20Adapter.spec.ts
@@ -7,21 +7,19 @@ import {
   shouldBehaveLikeErc20AdapterDeposit,
   shouldBehaveLikeErc20AdapterInitialize,
   shouldBehaveLikeErc20AdapterQueries,
-  shouldBehaveLikeErc20AdapterWithdraw,
-  shouldBehaveLikeErc20AdapterWithdrawAll,
-  shouldBehaveLikeErc20AdapterWithdrawUnderlying,
-  shouldBehaveLikeErc20AdapterWithdrawUnderlyingUpTo
 } from "../Erc20AdapterBehavior.spec"
-import { advanceBlock, deployContract, getIERC20, CompoundConverter } from '../shared'
+import { advanceBlock, deployContract, getIERC20, CompoundConverter, sendTokenToFrom, getBigNumber, createBalanceCheckpoint } from '../shared'
 
 describe('CErc20Adapter', () => {
   let comp: IERC20
+  let lens: TestComptrollerLens;
 
   before(async () => {
     comp = await getIERC20('0xc00e94cb662c3520282e6f5717214004a7f26888')
+    lens = await deployContract<TestComptrollerLens>('TestComptrollerLens')
   })
 
-  const getLens = () => deployContract<TestComptrollerLens>('TestComptrollerLens')
+  const getPendingRewards = (cToken: string, account: string) => lens.callStatic.getPendingRewards(account, cToken, { blockTag: 'pending' })
 
   const testAdapter = (_underlying: string, _ctoken: string, symbol: string) => describe(`c${symbol}`, function () {
     setupAdapterContext(
@@ -41,15 +39,32 @@ describe('CErc20Adapter', () => {
       shouldBehaveLikeErc20AdapterDeposit()
     })
   
-    describe('withdraw()', function () {
-      shouldBehaveLikeErc20AdapterWithdraw()
+    describe('withdraw()', () => {
+      beforeEach(function () {return this.resetTests(true);})
+
+      it('Should revert if caller has insufficient balance', async function () {
+        await expect(this.adapter.connect(this.wallet1).withdraw(getBigNumber(1))).to.be.revertedWith('TH:STF')
+      })
+    
+      it('Should burn wrapper and redeem underlying', async function () {
+        const balance = await this.wrapper.balanceOf(this.depositReceiverWrapped, { blockTag: 'pending' })
+        const balanceValue = await this.toUnderlying(balance)
+        let expectedOutput = balanceValue
+        if (this.symbol === 'COMP') {
+          expectedOutput = expectedOutput.add(await getPendingRewards(this.wrapper.address, this.wallet.address))
+        }
+        await expect(this.adapter.withdraw(balance))
+          .to.emit(this.underlying, 'Transfer')
+          .withArgs(this.withdrawalSenderUnderlying, this.wallet.address, balanceValue)
+        expect(await this.underlying.balanceOf(this.wallet.address)).to.eq(expectedOutput)
+      })
 
       it('Should claim COMP owed to caller if incentivized', async function () {
         await this.resetTests(true)
         const comptroller = (await ethers.getContractAt('IComptroller', '0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B')) as IComptroller
         if ((await comptroller.compSpeeds(this.wrapper.address)).gt(0)) {
           await advanceBlock()
-          const expectedRewards = await (await getLens()).callStatic.getPendingRewards(this.wallet.address, this.wrapper.address, { blockTag: 'pending' })
+          const expectedRewards = await getPendingRewards(this.wrapper.address, this.wallet.address)
           expect(expectedRewards).to.be.gt(0)
           await expect(this.adapter.withdraw(await this.wrapper.balanceOf(this.wallet.address)))
             .to.emit(comp, 'Transfer')
@@ -59,14 +74,27 @@ describe('CErc20Adapter', () => {
     })
   
     describe('withdrawAll()', function () {
-      shouldBehaveLikeErc20AdapterWithdrawAll()
+      beforeEach(function () {return this.resetTests(true);})
+
+      it('Should burn all caller wrapper token and redeem underlying', async function () {
+        const wBalance = await this.wrapper.balanceOf(this.depositReceiverWrapped, { blockTag: 'pending' })
+        const getBalanceChange = await createBalanceCheckpoint(this.underlying, this.wallet.address)
+        let expectedOutput = await this.toUnderlying(wBalance)
+        if (this.symbol === 'COMP') {
+          expectedOutput = expectedOutput.add(await getPendingRewards(this.wrapper.address, this.wallet.address))
+        }
+        await this.adapter.withdrawAll()
+        const balanceChange = await getBalanceChange()
+        expect(balanceChange).to.eq(expectedOutput)
+        expect(await this.wrapper.balanceOf(this.wallet.address)).to.eq(0)
+      })
 
       it('Should claim COMP owed to caller if incentivized', async function () {
         await this.resetTests(true)
         const comptroller = (await ethers.getContractAt('IComptroller', '0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B')) as IComptroller
         if ((await comptroller.compSpeeds(this.wrapper.address)).gt(0)) {
           await advanceBlock()
-          const expectedRewards = await (await getLens()).callStatic.getPendingRewards(this.wallet.address, this.wrapper.address, { blockTag: 'pending' })
+          const expectedRewards = await getPendingRewards(this.wrapper.address, this.wallet.address)
           expect(expectedRewards).to.be.gt(0)
           await expect(this.adapter.withdrawAll())
             .to.emit(comp, 'Transfer')
@@ -76,14 +104,31 @@ describe('CErc20Adapter', () => {
     })
   
     describe('withdrawUnderlying()', function () {
-      shouldBehaveLikeErc20AdapterWithdrawUnderlying()
+      beforeEach(function () {return this.resetTests(true);})
+
+      it('Should revert if caller has insufficient balance', async function () {
+        await expect(this.adapter.connect(this.wallet1).withdrawUnderlying(getBigNumber(1))).to.be.revertedWith('TH:STF')
+      })
+    
+      it('Should burn wrapper and redeem underlying', async function () {
+        const balanceUnderlying = await this.adapter.balanceUnderlying({ blockTag: 'pending' })
+        let expectedOutput = balanceUnderlying;
+        if (this.symbol === 'COMP') {
+          expectedOutput = expectedOutput.add(await getPendingRewards(this.wrapper.address, this.wallet.address))
+        }
+        await expect(this.adapter.withdrawUnderlying(balanceUnderlying))
+          .to.emit(this.underlying, 'Transfer')
+          .withArgs(this.withdrawalSenderUnderlying, this.wallet.address, balanceUnderlying)
+        expect(await this.underlying.balanceOf(this.wallet.address)).to.eq(expectedOutput)
+        expect(await this.wrapper.balanceOf(this.adapter.address)).to.eq(0)
+      })
 
       it('Should claim COMP owed to caller if incentivized', async function () {
         await this.resetTests(true)
         const comptroller = (await ethers.getContractAt('IComptroller', '0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B')) as IComptroller
         if ((await comptroller.compSpeeds(this.wrapper.address)).gt(0)) {
           await advanceBlock()
-          const expectedRewards = await (await getLens()).callStatic.getPendingRewards(this.wallet.address, this.wrapper.address, { blockTag: 'pending' })
+          const expectedRewards = await getPendingRewards(this.wrapper.address, this.wallet.address)
           expect(expectedRewards).to.be.gt(0)
           await expect(this.adapter.withdrawUnderlying(await this.adapter.balanceUnderlying()))
             .to.emit(comp, 'Transfer')
@@ -93,14 +138,38 @@ describe('CErc20Adapter', () => {
     })
   
     describe('withdrawUnderlyingUpTo()', function () {
-      shouldBehaveLikeErc20AdapterWithdrawUnderlyingUpTo()
+      beforeEach(async function () {
+        await this.resetTests()
+        await sendTokenToFrom(this.underlying, await this.converter.liquidityHolder(this.wrapper), `0x${'ff'.repeat(20)}`, await this.adapter.availableLiquidity({ blockTag: 'pending' }))
+        await this.adapter.deposit(this.amountDeposited)
+      })
+    
+      it('Should revert if caller has insufficient balance', async function () {
+        await expect(this.adapter.connect(this.wallet1).withdrawUnderlyingUpTo(getBigNumber(1))).to.be.revertedWith('TH:STF')
+      })
+    
+      it('Should withdraw min(amount, available)', async function () {
+        const balanceUnderlying = await this.adapter.balanceUnderlying({ blockTag: 'pending' })
+        const halfBalance = balanceUnderlying.div(2)
+        await sendTokenToFrom(this.underlying, await this.converter.liquidityHolder(this.wrapper), `0x${'ff'.repeat(20)}`, halfBalance)
+        const available = await this.adapter.availableLiquidity({ blockTag: 'pending' })
+        let expectedOutput = available;
+        if (this.symbol === 'COMP') {
+          expectedOutput = expectedOutput.add(await getPendingRewards(this.wrapper.address, this.wallet.address))
+        }
+        await expect(this.adapter.withdrawUnderlyingUpTo(balanceUnderlying))
+          .to.emit(this.underlying, 'Transfer')
+          .withArgs(this.withdrawalSenderUnderlying, this.wallet.address, available)
+        expect(await this.underlying.balanceOf(this.wallet.address)).to.eq(expectedOutput)
+        expect(await this.wrapper.balanceOf(this.adapter.address)).to.eq(0)
+      })
 
       it('Should claim COMP owed to caller if incentivized', async function () {
         await this.resetTests(true)
         const comptroller = (await ethers.getContractAt('IComptroller', '0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B')) as IComptroller
         if ((await comptroller.compSpeeds(this.wrapper.address)).gt(0)) {
           await advanceBlock()
-          const expectedRewards = await (await getLens()).callStatic.getPendingRewards(this.wallet.address, this.wrapper.address, { blockTag: 'pending' })
+          const expectedRewards = await getPendingRewards(this.wrapper.address, this.wallet.address)
           expect(expectedRewards).to.be.gt(0)
           await expect(this.adapter.withdrawUnderlyingUpTo(await this.adapter.balanceUnderlying()))
             .to.emit(comp, 'Transfer')
