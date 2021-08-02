37665083d4c86a20426642803a8abcc760d638fa
d1ll0n <dillonkellar@protonmail.com>
2021-08-02 12:08:25 -0500

    Add test for batch rebalancer

--- test/shared/fixtures.ts
+++ test/shared/fixtures.ts
@@ -1,7 +1,7 @@
 import { BigNumber, constants } from "ethers";
 import { waffle } from "hardhat";
-import { AdapterRegistry, TestAdapter, TestERC20, TestVault } from "../../typechain";
-import { deployContract, getBigNumber, getContract } from "./utils";
+import { AdapterRegistry, TestAdapter, TestERC20, TestNirnVault, TestVault } from "../../typechain";
+import { deployClone, deployContract, getBigNumber, getContract } from "./utils";
 
 export async function deployTestWrapperAndAdapter(
   underlying: string,
@@ -14,6 +14,42 @@ export async function deployTestWrapperAndAdapter(
 
 export function deployTestERC20(name: string = 'Test Token', symbol: string = 'TOK', initBalance: BigNumber = getBigNumber(100)): Promise<TestERC20> {
   return deployContract('TestERC20', name, symbol, initBalance);
+}
+
+export async function deployTestVaultStack(
+  registry: AdapterRegistry,
+  eoaSafeCaller = constants.AddressZero,
+  name: string = 'Test Token 2',
+  symbol: string = 'TOK2',
+  initBalance: BigNumber = getBigNumber(150)
+) {
+  const [owner,,protocolAdapter, vaultFactory] = waffle.provider.getWallets()
+  const underlying: TestERC20 = await deployContract('TestERC20', name, symbol, initBalance);
+  const { adapter: adapter1 } = await deployTestWrapperAndAdapter(underlying.address, getBigNumber(1, 17))
+  const { adapter: adapter2 } = await deployTestWrapperAndAdapter(underlying.address, getBigNumber(5, 16))
+  await underlying.approve(adapter1.address, constants.MaxUint256)
+  await underlying.approve(adapter2.address, constants.MaxUint256)
+  await adapter1.deposit(initBalance.div(3))
+  await adapter2.deposit(initBalance.div(3))
+  if ((await registry.protocolAdapterIds(protocolAdapter.address)).eq(0)) {
+    await registry.addProtocolAdapter(protocolAdapter.address)
+  }
+  await registry.connect(protocolAdapter).addTokenAdapters([ adapter1.address, adapter2.address ])
+  if (!(await registry.approvedVaultFactories(vaultFactory.address))) {
+    await registry.addVaultFactory(vaultFactory.address)
+  }
+  const implementation = await deployContract<TestNirnVault>('TestNirnVault', registry.address, eoaSafeCaller)
+  const vault = await deployClone(implementation)
+  await vault.initialize(underlying.address, owner.address, owner.address, owner.address)
+  await registry.connect(vaultFactory).addVault(vault.address)
+  await underlying.approve(vault.address, constants.MaxUint256)
+  await underlying.mint(owner.address, getBigNumber(4))
+  return {
+    adapter1,
+    adapter2,
+    underlying,
+    vault
+  }
 }
 
 export async function deployTestAdaptersAndRegistry(name: string = 'Test Token', symbol: string = 'TOK', initBalance: BigNumber = getBigNumber(150), approveAdapters = true) {
